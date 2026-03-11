fe7a70bdd0b574dd8ec67769600808a823e29781
1measure-7cuts <aleks.octocat@gmail.com>
2026-03-12 02:13:37 +0700

    start working on task3

--- course_2/OSBasics-labs/lab2-syscalls/task3/main.c
+++ course_2/OSBasics-labs/lab2-syscalls/task3/main.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h> // for exit
 #include <unistd.h> // for fork, execve
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#define IGNORED 0
 
 int main() {
+    const int ignored = 0;
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork problem");
         exit(1);
     } else if (pid == 0) {
-        execve("./hello_static", NULL, NULL);
+        ptrace(PTRACE_ME, 0, 0, 0);
+        execve("./hello_static", IGNORED, IGNORED);
         perror("execve problem"); /* execve has no return. if we are here -- execve has problems */
         exit(2);
     }
-
     /* only parent process should be heere */
-    puts("ima parent");  
+
 }
