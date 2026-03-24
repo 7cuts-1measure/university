7163d206ae52c6bc874ff1de54b283a85b0c969c
7cuts-1measure <aleks.octocat@gmail.com>
2026-03-24 23:10:48 +0700

    упростил фильтр

--- course_2/NetworkBasics-labs/lab4-eBPF/filter.c
+++ course_2/NetworkBasics-labs/lab4-eBPF/filter.c
@@ -1,4 +1,4 @@
-#include <bits/sockaddr.h>
+#include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>         // struct iphdr 
@@ -16,6 +16,7 @@
 #include <linux/if_ether.h>
 #include <net/if.h>
 #include <stdbool.h>
+#include <err.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 #define BUF_SIZE 65536
@@ -30,23 +31,11 @@ typedef struct ifreq ifreq;
 typedef struct ether_header ether_header;
 typedef struct iphdr iphdr; // internet address (ipv4)
 
-typedef enum {
-    DENY = 0,
-    ALLOW = 1,
-} Action;
 
-typedef struct {
-    uint32_t source_ip;
-    uint32_t dest_ip;
-    Action action; 
-} FilterRule;
 // ====================================================== //
 
 
 // ===============vars declaration======================= //
-bool running = true;
-char buf[BUF_SIZE];
-
 // Программа-фильтр
 // код получается с помощью `tcpdump -dd ip`
 // фильтр читает пакеты и если это ipv4, то копирует 256 КБ пакета
@@ -69,29 +58,25 @@ sock_filter tcp_ip_filter_code[] = {
     { 0x6, 0, 0, 0x00040000 },
     { 0x6, 0, 0, 0x00000000 },
 };
-
-// Структура для setsockopt(2)
-sock_fprog prog = {
-    .len = ARRAY_SIZE(ip_filter_code),
-    .filter = ip_filter_code
-};
 // ====================================================== //
 
 
-void log_ethernet_packet(unsigned long packet_count, size_t len, ether_header *eth) 
-{
+void log_ethernet_packet(size_t len, ether_header *eth) 
+{  
+    static size_t cnt = 0;
     if (!IS_LOGGING) return;
     printf("[%lu] Packet: %ld bytes\n"
             "\tEtherType: 0x%04x\n"
             "\tSRC MAC: %02x:%02x:%02x:%02x:%02x:%02x\n"
             "\tDST MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-            packet_count, len,
+            cnt, len,
             ntohs(eth->ether_type),
             eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],
             eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5],
             eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],
             eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]
     );
+    cnt++;
 }
 void log_ip_header(iphdr *ip) 
 {
@@ -115,9 +100,17 @@ void log_ip_protocol(iphdr *ip)
     }
 }
 
-// процедура главная
-int main() 
-{
+void log_packet(size_t packet_len, ether_header *eth) {
+    iphdr *ip = (iphdr *) (eth + 1);
+    log_ethernet_packet(packet_len, eth);
+    log_ip_header(ip);
+    log_ip_protocol(ip);
+}
+
+int setup_filter(sock_fprog *prog) {
+    if (prog == NULL) {
+        errx(EXIT_FAILURE, "Error: Program for eBPF is NULL");
+    }
     // для bind(2). привязка сокета к интерфейсу eth0
     sockaddr_ll addr = {
         .sll_family = AF_PACKET,
@@ -125,51 +118,46 @@ int main()
         .sll_ifindex = if_nametoindex("wlan0"),
         .sll_pkttype = PACKET_HOST  // только пакеты для этого хоста
     };
-
-
     // создаём сокет
-    // TODO: комментарий дипсека
     // AF_PACKET - работа на канальном уровне
     int socket_fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (socket_fd == -1) {
         perror("Cannot open socket");
         exit(EXIT_FAILURE);
     }
-
     // связываем интерфес c сокетом
     if (bind(socket_fd, (sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("Connot bind interface wlan0 to socket");
         exit(EXIT_FAILURE);
     }
-
     // применяем BPF фильтр
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_ATTACH_FILTER, &prog, sizeof(prog)) == -1) {
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_ATTACH_FILTER, prog, sizeof(*prog)) == -1) {
         perror("Cannot set bpf-filter to socket");
         exit(EXIT_FAILURE);
     }
-    
-    // bufer выглядит так
-    //      14            ???           2                2
-    //ethernet header | ip header | source port | destination port
-    unsigned long packet_count = 0; 
-    while (running) {
-        ssize_t packet_len = recv(socket_fd, buf, sizeof(buf), 0);
+    return socket_fd;
+}
+
+
+// процедура главная
+int main() 
+{   
+    sock_fprog prog = {
+        .len = ARRAY_SIZE(tcp_ip_filter_code),
+        .filter = tcp_ip_filter_code
+    };
+
+    int socket_fd = setup_filter(&prog);
+    char packet_buf[BUF_SIZE];
+    while (1) {
+        ssize_t packet_len = recv(socket_fd, packet_buf, sizeof(packet_buf), 0);
         if (packet_len == -1) {
-            perror("Error while read data from socket");
+            perror("Error while reading data from socket");
             exit(EXIT_FAILURE);
         }
-        packet_count++;
-        // читаем ethernet header
-        ether_header *eth = (ether_header *) buf;
-        log_ethernet_packet(packet_count, packet_len, eth);
-        
-        // читаем ip header (он сразу после ethernet хедера)
-        iphdr *ip = (iphdr *) (buf + sizeof(ether_header));  
-        log_ip_header(ip);
-        log_ip_protocol(ip);
-        if (ip->protocol == IPPROTO_ICMP) {
-            break;
-        }
+        ether_header *eth = (ether_header *) packet_buf;
+        iphdr *ip = (iphdr *) (eth + 1);  // ip header goes after the end of ethernet header       
+        log_packet(packet_len, eth);
     }
     
     printf("done!\n");
