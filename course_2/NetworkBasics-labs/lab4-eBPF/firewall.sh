b8b7f649fbbaabb232d4f6ba0a8aad9ee886b007
7cuts-1measure <aleks.octocat@gmail.com>
2026-04-07 18:53:49 +0700

    refactor script

--- course_2/NetworkBasics-labs/lab4-eBPF/firewall.sh
+++ course_2/NetworkBasics-labs/lab4-eBPF/firewall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# !!скрипт написан чатом гпт с небольшими ручными правками!!
+
+
 # ========== НАСТРОЙКИ ==========
 BPF_OBJ="filter.o"             # объектный файл eBPF
 BPF_SRC="filter.c"             # исходный код eBPF
@@ -26,35 +29,20 @@ check_deps() {
 # Преобразование IP (a.b.c.d) в шестнадцатеричный формат для bpftool (big-endian)
 ip_to_hex() {
     local ip="$1"
-    local hex=""
     IFS='.' read -r o1 o2 o3 o4 <<< "$ip"
     printf "%02x %02x %02x %02x" "$o1" "$o2" "$o3" "$o4"
 }
 
 # Преобразование шестнадцатеричного вывода bpftool в читаемый IP
-# На вход: строка вида "c0 a8 01 01" (без кавычек)
-hex_to_ip() {
-    local hex_str="$1"
-    # Удаляем все пробелы, переводим в верхний регистр для единообразия
-    hex_str=$(echo "$hex_str" | tr -d ' ' | tr '[:lower:]' '[:upper:]')
-    # Разбиваем по два символа
-    local oct1=$((16#${hex_str:0:2}))
-    local oct2=$((16#${hex_str:2:2}))
-    local oct3=$((16#${hex_str:4:2}))
-    local oct4=$((16#${hex_str:6:2}))
-    echo "$oct1.$oct2.$oct3.$oct4"
-}
+
 
 # Получить ID карты по имени (через bpftool)
 get_map_id() {
     id_with_column=$(sudo bpftool map list | grep  "name blacklist" | grep -oP '\d+:')
-    echo ${id_with_column::-1}
+    echo "${id_with_column::-1}"
 }
 
 # Проверить, загружена ли XDP-программа на интерфейс
-is_xdp_loaded() {
-    ip link show "$INTERFACE" | grep -q "xdp"
-}
 
 # ========== ОСНОВНЫЕ КОМАНДЫ ==========
 block_ip() {
@@ -80,11 +68,21 @@ unblock_ip() {
         echo -e "${RED}ERROR: map $MAP_NAME is not found.${NC}"
         return 1
     fi
-    if sudo bpftool map delete id "$map_id" key hex $hex_key; then
+    if sudo bpftool map delete id "$map_id" key hex "$hex_key"; then
         echo -e "${GREEN}$ip was deleted from blacklist.${NC}"
     else
         echo -e "${YELLOW}Cannot delete $ip (may be, it is not in the blacklist).${NC}"
     fi
+}
+
+decimal_to_ip_with_dots() {
+    num=$1
+    # преобразуем десятичное число в IP (host order, little-endian)
+    local oct1=$((num & 0xFF))
+    local oct2=$(((num >> 8) & 0xFF))
+    local oct3=$(((num >> 16) & 0xFF))
+    local oct4=$(((num >> 24) & 0xFF))
+    echo "$oct1.$oct2.$oct3.$oct4"
 }
 
 list_blacklist() {
@@ -100,15 +98,9 @@ list_blacklist() {
         return
     fi
     echo -e "${GREEN}Blacklist:${NC}"
-    # Парсим строки вида "key: c0 a8 01 01 value: 00"
     
     echo "$dump" | grep -oP '\"key\": \d+' | awk -F ': ' '{print $2}' | while read -r num; do
-        # преобразуем десятичное число в IP (host order, little-endian)
-        local oct1=$((num & 0xFF))
-        local oct2=$(((num >> 8) & 0xFF))
-        local oct3=$(((num >> 16) & 0xFF))
-        local oct4=$(((num >> 24) & 0xFF))
-        echo "  $oct1.$oct2.$oct3.$oct4"
+        echo "   $(decimal_to_ip_with_dots $num)"
     done
 }
 
@@ -123,32 +115,31 @@ rebuild_xdp() {
 
 load_xdp() {
     if ! ip link show "$INTERFACE" &>/dev/null; then
-        echo -e "${RED}ERROR: Interface "$INTERFACE" does not exist.${NC}"
+        echo -e "${RED}ERROR: Interface $INTERFACE does not exist.${NC}"
         exit 1
     fi
     
-    echo "Load XDP on "$INTERFACE" ..."
-    # Открепляем старую программу, если была
+    echo "Load XDP on $INTERFACE ..."
     sudo ip link set dev "$INTERFACE" xdp off
     sudo ip link set dev "$INTERFACE" xdp obj "$BPF_OBJ" sec xdp
     
     if [[ $? -eq 0 ]]; then
-        echo -e "${GREEN}XDP loaded on "$INTERFACE".${NC}"
+        echo -e "${GREEN}XDP loaded on $INTERFACE.${NC}"
     else
-        echo -e "${RED}ERROR: Can't load XDP on "$INTERFACE"."
+        echo -e "${RED}ERROR: Can't load XDP on $INTERFACE."
         exit 1
     fi
 }
 
 unload_xdp() {
-    echo "Unloading XDP from "$INTERFACE" ..."
+    echo "Unloading XDP from $INTERFACE ..."
     sudo ip link set dev "$INTERFACE" xdp off
     echo -e "${GREEN}XDP unloaded.${NC}"
     exit
 }
 
 interactive_loop() {
-    echo -e "${GREEN}XDP Firewall on "$INTERFACE" is active.${NC}"
+    echo -e "${GREEN}XDP Firewall on $INTERFACE is active.${NC}"
     echo "Enter help for get list of commands."
     while true; do
         read -r -p "> " cmd arg
