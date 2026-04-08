a47f9509d79fecd5e6e8a30baa3374bf432284c1
7cuts-1measure <aleks.octocat@gmail.com>
2026-04-08 17:46:41 +0700

    change constatns

--- course_2/NetworkBasics-labs/lab4-eBPF/spam_filter.c
+++ course_2/NetworkBasics-labs/lab4-eBPF/spam_filter.c
@@ -7,11 +7,10 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-#define MAX_PAYLOAD_LEN 1639
+#define MAX_PAYLOAD_LEN 1600
 
 // Helper function to check if the packet is TCP
 static bool is_tcp(struct ethhdr *eth, void *data_end) {
-    // Ensure Ethernet header is within bounds
     if ((void *)(eth + 1) > data_end)
         return false;
 
@@ -21,11 +20,9 @@ static bool is_tcp(struct ethhdr *eth, void *data_end) {
 
     struct iphdr *ip = (struct iphdr *)(eth + 1);
 
-    // Ensure IP header is within bounds
     if ((void *)(ip + 1) > data_end)
         return false;
 
-    // Check if the protocol is TCP
     if (ip->protocol != IPPROTO_TCP)
         return false;
 
@@ -36,40 +33,25 @@ static bool is_tcp(struct ethhdr *eth, void *data_end) {
 
 SEC("xdp")
 int xdp_pass(struct xdp_md *ctx) {
+    
     // Pointers to packet data
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
-    // Parse Ethernet header
     struct ethhdr *eth = data;
 
-    // Check if the packet is a TCP packet
     if (!is_tcp(eth, data_end))                             return XDP_PASS;
 
-    // Cast to IP header
     struct iphdr *ip = (struct iphdr *)(eth + 1);
-
-    // Calculate IP header length
     int ip_hdr_len = ip->ihl * 4;
-    if (ip_hdr_len < sizeof(struct iphdr))                  return XDP_PASS;
-    
 
-    // Ensure IP header is within packet bounds
-    if ((void *)ip + ip_hdr_len > data_end)                 return XDP_PASS;
-
-
-    // Parse TCP header
     struct tcphdr *tcp = (struct tcphdr *)((unsigned char *)ip + ip_hdr_len);
-
-    // Ensure TCP header is within packet bounds
     if ((void *)(tcp + 1) > data_end)                       return XDP_PASS;
+    
     unsigned int tcp_hdr_len = tcp->doff * 4;
     if (tcp_hdr_len < sizeof(struct tcphdr))                return XDP_PASS;
     
     unsigned char *payload = (unsigned char *)tcp + tcp_hdr_len;
-    const long payload_len = (char *)data_end - (char *)payload;
-    if (payload_len < 4)                                    return XDP_PASS;
-
 
     for (int i = 0; i <= MAX_PAYLOAD_LEN-4; i++) {
         if ((void *)(payload + i + 4) > data_end) break;
