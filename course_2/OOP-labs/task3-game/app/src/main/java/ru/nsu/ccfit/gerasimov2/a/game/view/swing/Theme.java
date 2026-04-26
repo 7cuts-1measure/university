20ad3dd6bb1844d9fc5e42dc60d13e98e99d44ad
7cuts-1measure <aleks.octocat@gmail.com>
2026-04-27 01:41:53 +0700

    remove unused import

--- course_2/OOP-labs/task3-game/app/src/main/java/ru/nsu/ccfit/gerasimov2/a/game/view/swing/Theme.java
+++ course_2/OOP-labs/task3-game/app/src/main/java/ru/nsu/ccfit/gerasimov2/a/game/view/swing/Theme.java
@@ -4,10 +4,10 @@ import java.awt.Color;
 
 public class Theme {
     public static final Color BACKGROUND_DARK = new Color(35, 35, 35);
-    public static final Color BLUE = new Color(135, 191, 180);
-    public static final Color WHITE = new Color(247, 238, 193);
-    public static final Color RED = new Color(247, 97, 96);
-    public static final Color ORANGE = new Color(247, 182, 102);
-    public static final Color GREEN = new Color(172, 240, 152);
-    public static final Color GRAY = new Color(135, 191, 180);
+    public static final Color BLUE            = new Color(135, 191, 180);
+    public static final Color WHITE           = new Color(247, 238, 193);
+    public static final Color RED             = new Color(247, 97, 96);
+    public static final Color ORANGE          = new Color(247, 182, 102);
+    public static final Color GREEN           = new Color(172, 240, 152);
+    public static final Color GRAY            = new Color(135, 191, 180);
 }
