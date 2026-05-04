770090aa1cda96e22514e930f70515704eee5832
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-04 19:36:16 +0700

    add default value for performance in supplier

--- course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/factory/Supplier.java
+++ course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/factory/Supplier.java
@@ -6,38 +6,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Supplier<T> extends Thread {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    
+    private static final int DEFAULT_PERFORMANCE = 5;
+
+    private static final Logger log = LoggerFactory.getLogger(Supplier.class);
+
     private final IdGenerator idGenerator = new IdGenerator();
+
     private final Creator<T> creator;
+
     private final Storage<T> storage;
 
-    private AtomicInteger performance;
-    
-    public void setPerformance(int performance) {
-        this.performance.set(performance);
-    }
+    // details per second
+    private final AtomicInteger performance = new AtomicInteger(DEFAULT_PERFORMANCE);
 
-    public Supplier(int performance, Storage<T> storage, Creator<T> creator) {
-        this.performance = new AtomicInteger();
-        this.performance.set(performance);
+    public Supplier(Storage<T> storage, Creator<T> creator) {
+        this.performance.set(DEFAULT_PERFORMANCE);
         this.storage = storage;
         this.creator = creator;
-    }
-
-    private void supplyStorage() throws InterruptedException {
-        T product = creator.newProduct(idGenerator.next());
-        storage.put(product);
     }
 
     @Override
     public void run() {
         try {
             while (true) {
-                sleep(sleepDuration());
                 supplyStorage();
+                sleep(sleepDuration());
             }
         } catch (InterruptedException e) {
             log.info("Thread was interrupted");
@@ -45,8 +39,22 @@ public class Supplier<T> extends Thread {
         }
     }
 
+    private void supplyStorage() throws InterruptedException {
+        if (log.isDebugEnabled()) log.debug(getName());
+        T product = creator.newProduct(idGenerator.next());
+        storage.put(product);
+    }
+
+    public void setPerformance(int performance) {
+        this.performance.set(performance);
+    }
+
     private Duration sleepDuration() {
         final int millisInSecond = 1000;
         return Duration.ofMillis(millisInSecond / performance.get());
+    }
+
+    public int getPerformance() {
+        return performance.get();
     }
 }
