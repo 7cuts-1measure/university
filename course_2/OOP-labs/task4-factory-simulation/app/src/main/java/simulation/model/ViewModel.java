b916343fbacb90523c8a7690821134af2384ed71
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-04 15:26:44 +0700

    create ViewModel interface

--- course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/ViewModel.java
+++ course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/ViewModel.java
@@ -4,5 +4,25 @@ package simulation.model;
  * Interface that view expects from model. It contains all methods to view for draw model state
  */
 public interface ViewModel {
+    int getMotorSupplierPerformance();
     
+    int getBodySupplierPerformance();
+    
+    int getAccessorySupplierPerformance();
+    int getNumAccessorySuppliers();
+    
+    int getMotorStorageSize();
+    int getMotorStorageCap();
+
+    int getBodySorageSize();
+    int getBodyStorageCap();
+
+    int getAccessoryStorageSize();
+    int getAccessoryStorageCap();
+
+    int getCarStorageSize();
+    int getCarStorageCap();
+
+    int getNumActiveWorkers();
+    int getNumTotalWorkets();
 }
