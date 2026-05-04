a2682ee037c8615d527cf7cea5d5c4a3e1838a3f
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-04 19:35:56 +0700

    accept capcity in storage

--- course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/factory/Storage.java
+++ course_2/OOP-labs/task4-factory-simulation/app/src/main/java/simulation/model/factory/Storage.java
@@ -8,7 +8,11 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class Storage<T> {
 
-    BlockingQueue<T> storage = new LinkedBlockingQueue<>();
+    private final BlockingQueue<T> storage;
+
+    public Storage(int capacity) {
+        storage = new LinkedBlockingQueue<>(capacity);
+    }
 
     public void put(T product) throws InterruptedException {
         storage.put(product);
