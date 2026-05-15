77cf40396b6fe8a39c068eb9015f20c232155d60
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-16 03:05:31 +0700

    ignore signals form other processes

--- course_2/OSBasics-labs/lab6-ipc/1/main.c
+++ course_2/OSBasics-labs/lab6-ipc/1/main.c
@@ -1,6 +1,5 @@
-#include <asm-generic/errno-base.h>
 #include <assert.h>
-#include <errno.h>
+#include <bits/types/siginfo_t.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -9,8 +8,10 @@
 #include <err.h>
 #include <fcntl.h>
 
-sig_atomic_t allow_read = false;
-sig_atomic_t allow_write = true;
+volatile sig_atomic_t allow_read = false;
+volatile sig_atomic_t pid_allow_read;
+volatile sig_atomic_t allow_write = true;
+volatile sig_atomic_t pid_allow_write;
 
 void check_order(unsigned num, unsigned prev_num) {
     if (num != prev_num + 1)
@@ -18,20 +19,61 @@ void check_order(unsigned num, unsigned prev_num) {
 }
 
 
-void allow_read_handler(int signo) {
+void allow_read_handler(int signo, siginfo_t *siginfo, void *context) {
+    
     if (signo == SIGUSR1) {
+        pid_allow_read = siginfo->si_pid;
         allow_read = true;
     }
-
 }
 
-void child_work(void *buf, size_t size, sigset_t old) {
+void allow_write_handler(int signo, siginfo_t *siginfo, void *context) {
+    if (signo == SIGUSR1) {
+        allow_write = true;
+        pid_allow_write = siginfo->si_pid;
+    }
+}
+
+unsigned child_recv_num(const unsigned *ptr, pid_t ppid, const sigset_t *old) {
+    while (!allow_read || pid_allow_read != ppid) {
+        sigsuspend(old);   // unblock SIGUSR1
+    }
+    
+    unsigned num = *ptr;
+    allow_read = false;
+
+    if (kill(ppid, SIGUSR1) == -1) {
+        err(1, "SIGUSR1 to parent (%d)", ppid);
+    }
+    return num;
+}
+
+
+void parent_send_num(unsigned *ptr, unsigned num,  pid_t cpid, sigset_t *old) {
+    *ptr = num;
+    allow_write = false;
+
+    if (kill(cpid, SIGUSR1) == -1) {
+        err(1, "SIGUSR1 to %d", cpid);
+    }
+    while (!allow_write || pid_allow_write != cpid) {
+        sigsuspend(old);   // unblock SIGUSR1
+    }
+}
+
+void child_work(void *buf, size_t size, sigset_t *old) {
     assert(size % sizeof(unsigned int) == 0);
-   
     pid_t ppid = getppid();
-    printf("child: %d\nparent: %d\n", getpid(), ppid);
-   
-    signal(SIGUSR1, allow_read_handler);
+    printf("child: %d\n" "parent: %d\n", getpid(), ppid);
+
+    
+    struct sigaction sa =  {
+        .sa_handler = NULL,
+        .sa_mask = 0,
+        .sa_flags = SA_SIGINFO,
+        .sa_sigaction = allow_read_handler
+    };
+    sigaction(SIGUSR1, &sa, NULL);
          
     unsigned prev_num = -1;
     unsigned num;
@@ -39,46 +81,29 @@ void child_work(void *buf, size_t size, sigset_t old) {
 
     const size_t elems = size / sizeof(num);
     for (int i = 0; ; i++) {
-        while (!allow_read) {
-            sigsuspend(&old);
-        }
-        
-        num = ptr[i % elems];
-        allow_read = false;
-
-        if (kill(ppid, SIGUSR1) == -1) {
-            err(1, "SIGUSR1 to parent (%d)", ppid);
-        }
-        
+        num = child_recv_num(&ptr[i % elems], ppid, old);
         check_order(num, prev_num);
         prev_num = num;
     }
 }
 
-void allow_write_handler(int signo) {
-    if (signo == SIGUSR1) {
-        allow_write = true;
-    }
-}
 
-void parent_work(void *buf, size_t size, pid_t cpid, sigset_t old) {
-    signal(SIGUSR1, allow_write_handler);
-    
+void parent_work(void *buf, size_t size, pid_t cpid, sigset_t *old) {
+    struct sigaction sa =  {
+        .sa_handler = NULL,
+        .sa_mask = 0,
+        .sa_flags = SA_SIGINFO,
+        .sa_sigaction = allow_write_handler
+    };
+    sigaction(SIGUSR1, &sa, NULL);
+    pid_allow_write = cpid;
+
     assert(size % sizeof(unsigned int) == 0);
     unsigned *ptr = buf; 
     
     const size_t elems = size / sizeof(unsigned);
     for (unsigned i = 0; ; i++) {
-        ptr[i % elems] = i;
-        allow_write = false;
-
-        if (kill(cpid, SIGUSR1) == -1) {
-            err(1, "SIGUSR1 to %d", cpid);
-        }
-        
-        while (!allow_write) {
-            sigsuspend(&old);   // unblock SIGUSR1
-        }
+        parent_send_num(&ptr[i % elems], i, cpid, old);
     }
 }
 
@@ -100,9 +125,9 @@ int main() {
         case -1:
             err(1, "fork");
         case 0:
-            child_work(buf, PAGE_SIZE, old);
+            child_work(buf, PAGE_SIZE, &old);
         default:
-            parent_work(buf, PAGE_SIZE, pid, old);
+            parent_work(buf, PAGE_SIZE, pid, &old);
     } 
     assert(0);
 }
