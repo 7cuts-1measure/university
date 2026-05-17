14eff06a5119203c2694630d672c3b170440d51e
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-18 02:22:46 +0700

    ready. set. java!

---
+++ course_2/OOP-labs/task5-internet/src/client/ClientApp.java
@@ -0,0 +1,9 @@
+package client;
+
+import static java.lang.System.out;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        out.println("clent hello!");
+    }
+}
---
+++ course_2/OOP-labs/task5-internet/src/server/ServerApp.java
@@ -0,0 +1,9 @@
+package server;
+
+import static java.lang.System.out;
+
+class ServerApp {
+    public static void main(String[] args) {
+        out.println("server hello");
+    }
+}
