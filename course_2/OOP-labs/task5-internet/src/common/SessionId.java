33a43b9c89e503527e7922eed7f065f14f4014a2
7cuts-1measure <aleks.octocat@gmail.com>
2026-05-19 15:29:10 +0700

    create: SessionId

---
+++ course_2/OOP-labs/task5-internet/src/common/SessionId.java
@@ -0,0 +1,13 @@
+package common;
+
+public class SessionId {
+    private final int id;
+
+    public SessionId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
